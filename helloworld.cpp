4add465d2ce7171778a1e83092d5d1536b5ce57e
Romar Cablao <romarcablao@gmail.com>
2020-09-10 08:40:35 +0800

    Update the code based on the lab requirement

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int main()
+main()
 {
-    cout << "Hello World!";
-    return 0;
+	string name;
+	int year;
+	
+	cout<<"Enter your name:";
+	cin>>name;
+	cout<<"Enter your current year:";
+	cin>>year;
+	
+	if(year==1){
+		cout<<"1st Year : ComFunds, ComProg1";
+	} else 	if(year==2){
+		cout<<"2nd Year: ComProg2, OOP1";
+	} else 	if(year==3){
+		cout<<"3rd Year: OOP2, DBMS";
+	}else 	if(year==4){
+		cout<<"4th Year: GameDev, Thesis";
+	}else{
+		cout<<"Your choice is invalid!!!";
+	}
+	
+	return 0;
 }
