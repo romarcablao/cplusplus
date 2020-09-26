535c425ac5ecb3d93a2bf71b4aad4023242bc95a
Romar Cablao <romarcablao@gmail.com>
2020-09-26 20:00:01 +0800

    Update Lab3

--- Lab3_Pointer.cpp
+++ Lab3_Pointer.cpp
@@ -1,35 +1,42 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-	
+int main()
+{
+
 	string item;
 	double total, *ptrTotal = &total;
 	char addMoreToCart;
 	double keyboard = 950.00, mouse = 890.00, monitor = 10599.00, webcam = 1500.50;
-	
-	while(addMoreToCart != 'N'){
-		cout<<"What item would you like to buy? ";
-		cin>>item;
-	
-		if(item=="keyboard"){
+
+	while (addMoreToCart != 'N')
+	{
+		cout << "What item would you like to buy? ";
+		cin >> item;
+
+		if (item == "keyboard")
+		{
 			*ptrTotal += keyboard;
-		}else if(item=="mouse"){
+		}
+		else if (item == "mouse")
+		{
 			*ptrTotal += mouse;
-		}else{
+		}
+		else
+		{
 			*ptrTotal += 0;
-		}	
-		
-		cout<<"Would you like to addmore item/s to your cart? [Y/N]:";
-		cin>>addMoreToCart;
+		}
+
+		cout << "Would you like to addmore item/s to your cart? [Y/N]:";
+		cin >> addMoreToCart;
 	}
 
-	cout<<"Total price is " <<*ptrTotal <<"Php\n";
-	
-	cout<<"Pointer address: " <<ptrTotal <<endl;
-	cout<<"Pointer value: " <<*ptrTotal <<endl;
-	cout<<"Total address: " <<&total <<endl;
-	cout<<"Total value: " <<total <<endl;
-	
+	cout << "Total price is " << *ptrTotal << "Php\n";
+
+	cout << "Pointer address: " << ptrTotal << endl;
+	cout << "Pointer value: " << *ptrTotal << endl;
+	cout << "Total address: " << &total << endl;
+	cout << "Total value: " << total << endl;
+
 	return 0;
 }
